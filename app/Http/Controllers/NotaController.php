665a2ee4acf0750341e3b84b34dd9f4b67016605
Caroalv <alvaradocarolinad2002@gmail.com>
2023-10-03 11:53:14 -0600

    agregue las sugerencias con ajax en el form de notas

--- app/Http/Controllers/NotaController.php
+++ app/Http/Controllers/NotaController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 use App\Models\Nota;
+use App\Models\Alumno;
+use App\Models\Curso;
+use App\Models\Profesor;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
@@ -9,11 +12,15 @@ class NotaController extends Controller
 {
     public function index()
     {
-        // Obtener la lista de profesores
-        $notas = DB::select('SELECT * FROM notas');
+        // Obtener la lista de notas con nombres de alumno, curso y profesor
+        $notas = DB::table('notas')
+            ->select('notas.*', 'alumnos.nombre', 'alumnos.apellido', 'cursos.nombrecurso', 'profesor.nombre as nombre_profesor', 'profesor.apellido as apellido_profesor')
+            ->join('alumnos', 'notas.alumno_id', '=', 'alumnos.id')
+            ->join('cursos', 'notas.cursos_id', '=', 'cursos.id')
+            ->join('profesor', 'notas.profesor_id', '=', 'profesor.id')
+            ->get();
 
-        // Retornar la vista con los datos
-        return view('notas.index', ['notas' => $notas]); // Corrige el nombre de la vista
+        return view('notas.index', ['notas' => $notas]);
     }
 
     public function store(Request $request)
@@ -29,12 +36,65 @@ class NotaController extends Controller
             'alumno_id' => 'required|numeric',
             'cursos_id' => 'required|numeric',
             'profesor_id' => 'required|numeric',
-
         ]);
 
-        // Insertar los datos en la tabla de profesores utilizando el método insert
-        DB::table('notas')->insert($data); // Corrige el nombre de la tabla
+        // Insertar los datos en la tabla de notas utilizando el método insert
+        DB::table('notas')->insert([
+            'nota1' => $data['nota1'],
+            'nota2' => $data['nota2'],
+            'nota3' => $data['nota3'],
+            'nota4' => $data['nota4'],
+            'promedio' => $data['promedio'],
+            'parcial' => $data['parcial'],
+            'alumno_id' => $data['alumno_id'],
+            'cursos_id' => $data['cursos_id'],
+            'profesor_id' => $data['profesor_id'],
+        ]);
 
         return redirect('/notas');
+    }
+
+    public function buscarAlumnos(Request $request)
+    {
+        $searchTerm = $request->input('searchTerm');
+        $alumnos = Alumno::where('nombre', 'LIKE', "%$searchTerm%")
+                         ->orWhere('apellido', 'LIKE', "%$searchTerm%")
+                         ->get();
+
+        $html = '';
+        foreach ($alumnos as $alumno) {
+            $html .= "<li data-id='{$alumno->id}'>{$alumno->nombre} {$alumno->apellido}</li>";
+        }
+
+        return $html;
+    }
+
+    public function buscarCursos(Request $request)
+    {
+        $searchTerm = $request->input('searchTerm');
+        $cursos = Curso::where('nombrecurso', 'LIKE', "%$searchTerm%")
+                       ->get();
+
+        $html = '';
+        foreach ($cursos as $curso) {
+            $html .= "<li data-id='{$curso->id}'>{$curso->nombrecurso}</li>";
+        }
+
+        return $html;
+    }
+
+    public function buscarProfesores(Request $request)
+    {
+        $searchTerm = $request->input('searchTerm');
+        $profesores = Profesor::where('nombre', 'LIKE', "%$searchTerm%")
+                             ->orWhere('apellido', 'LIKE', "%$searchTerm%")
+                             ->get();
+
+        $html = '';
+        foreach ($profesores as $profesor) {
+            $html .= "<li data-id='{$profesor->id}'>{$profesor->nombre} {$profesor->apellido}</li>";
+        }
+
+        return $html;
     }
 }
--- resources/views/notas/index.blade.php
+++ resources/views/notas/index.blade.php
@@ -1,95 +1,180 @@
 @extends('layouts.app')
 
-
 @section('content')
-            <!-- Formulario para agregar nuevas notas en una caja -->
-            <div class="card mt-3">
-                <div class="card-body">
-                    <h2>Agregar notas mediante Insert</h2>
-                    <div class="container my-form">
-                        <!-- Formulario para agregar nuevas notas -->
-                        <form method="POST" action="/notas">
-                            @csrf
-                            <div class="form-group">
-                                <label for="nota1">Ingrese la nota 1:</label>
-                                <input type="text" name="nota1" class="form-control" required>
-                            </div>           
-                            <div class="form-group">
-                                <label for="nota2">Ingrese la nota 2:</label>
-                                <input type="text" name="nota2" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="nota3">Ingrese la nota 3:</label>
-                                <input type="text" name="nota3" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="nota4">Ingrese la nota 4:</label>
-                                <input type="text" name="nota4" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="promedio">Promedio:</label>
-                                <input type="text" name="promedio" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="parcial">Parcial:</label>
-                                <input type="text" name="parcial" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="alumno_id">Id alumno:</label>
-                                <input type="text" name="alumno_id" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="cursos_id">Id curso:</label>
-                                <input type="text" name="cursos_id" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="profesor_id">Id profesor:</label>
-                                <input type="text" name="profesor_id" class="form-control" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Agregar nota</button>
-                        </form>
+    <!-- Formulario para agregar nuevas notas en una caja -->
+    <div class="card mt-3">
+        <div class="card-body">
+            <h2>Agregar notas mediante Insert</h2>
+            <div class="container my-form">
+                <!-- Formulario para agregar nuevas notas -->
+                <form method="POST" action="/notas">
+                    @csrf
+                    <div class="form-group">
+                        <label for="nota1">Nota 1:</label>
+                        <input type="text" name="nota1" class="form-control" required>
+                    </div>           
+                    <div class="form-group">
+                        <label for="nota2">Nota 2:</label>
+                        <input type="text" name="nota2" class="form-control" required>
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label for="nota3">Nota 3:</label>
+                        <input type="text" name="nota3" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="nota4">Nota 4:</label>
+                        <input type="text" name="nota4" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="promedio">Promedio:</label>
+                        <input type="text" name="promedio" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="parcial">Parcial:</label>
+                        <input type="text" name="parcial" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="alumno_id">Alumno:</label>
+                        <input type="text" name="alumno_nombre" id="alumno_nombre" class="form-control" required>
+                        <ul id="alumno_sugerencias"></ul>
+                        <input type="hidden" name="alumno_id" id="alumno_id" value="">
+                    </div>
+                    <div class="form-group">
+                        <label for="cursos_id">Curso:</label>
+                        <input type="text" name="curso_nombre" id="curso_nombre" class="form-control" required>
+                        <ul id="curso_sugerencias"></ul>
+                        <input type="hidden" name="cursos_id" id="cursos_id" value="">
+                    </div>
+                    <div class="form-group">
+                        <label for="profesor_id">Profesor:</label>
+                        <input type="text" name="profesor_nombre" id="profesor_nombre" class="form-control" required>
+                        <ul id="profesor_sugerencias"></ul>
+                        <input type="hidden" name="profesor_id" id="profesor_id" value="">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Agregar nota</button>
+                </form>
+
+                <!-- script-->
+                <script>
+                    $(document).ready(function() {
+                        $('#alumno_nombre').on('keyup', function() {
+                            var searchTerm = $(this).val();
+                            if (searchTerm.length >= 0) {
+                                $.ajax({
+                                    url: "{{ route('notas.buscar_alumnos') }}",
+                                    type: 'POST',
+                                    data: {
+                                        _token: "{{ csrf_token() }}",
+                                        searchTerm: searchTerm
+                                    },
+                                    success: function(response) {
+                                        $('#alumno_sugerencias').html(response);
+                                    }
+                                });
+                            } else {
+                                $('#alumno_sugerencias').html('');
+                            }
+                        });
+
+                        $('#curso_nombre').on('keyup', function() {
+                            var searchTerm = $(this).val();
+                            if (searchTerm.length >= 0) {
+                                $.ajax({
+                                    url: "{{ route('notas.buscar_cursos') }}",
+                                    type: 'POST',
+                                    data: {
+                                        _token: "{{ csrf_token() }}",
+                                        searchTerm: searchTerm
+                                    },
+                                    success: function(response) {
+                                        $('#curso_sugerencias').html(response);
+                                    }
+                                });
+                            } else {
+                                $('#curso_sugerencias').html('');
+                            }
+                        });
+
+                        $('#profesor_nombre').on('keyup', function() {
+                            var searchTerm = $(this).val();
+                            if (searchTerm.length >= 0) {
+                                $.ajax({
+                                    url: "{{ route('notas.buscar_profesores') }}",
+                                    type: 'POST',
+                                    data: {
+                                        _token: "{{ csrf_token() }}",
+                                        searchTerm: searchTerm
+                                    },
+                                    success: function(response) {
+                                        $('#profesor_sugerencias').html(response);
+                                    }
+                                });
+                            } else {
+                                $('#profesor_sugerencias').html('');
+                            }
+                        });
+
+                        $('#alumno_sugerencias').on('click', 'li', function() {
+                            var alumnoNombre = $(this).text();
+                            var alumnoId = $(this).data('id');
+                            $('#alumno_nombre').val(alumnoNombre);
+                            $('#alumno_id').val(alumnoId);
+                            $('#alumno_sugerencias').html('');
+                        });
+
+                        $('#curso_sugerencias').on('click', 'li', function() {
+                            var cursoNombre = $(this).text();
+                            var cursoId = $(this).data('id');
+                            $('#curso_nombre').val(cursoNombre);
+                            $('#cursos_id').val(cursoId);
+                            $('#curso_sugerencias').html('');
+                        });
+
+                        $('#profesor_sugerencias').on('click', 'li', function() {
+                            var profesorNombre = $(this).text();
+                            var profesorId = $(this).data('id');
+                            $('#profesor_nombre').val(profesorNombre);
+                            $('#profesor_id').val(profesorId);
+                            $('#profesor_sugerencias').html('');
+                        });
+                    });
+                </script>
             </div>
         </div>
     </div>
-</div>
 
-
-<div class="container">
-    <h1>Lista de Notas con DB:: SELECT</h1>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Nota1</th>
-                <th>Nota2</th>
-                <th>Nota3</th>
-                <th>Nota4</th>
-                <th>promedio</th>
-                <th>parcial</th>
-                <th>id alumno</th>
-                <th>id_curso</th>
-                <th>id_profesor</th>
-
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($notas as $nota)
+    <!-- Lista de Notas -->
+    <div class="container mt-3">
+        <h1>Lista de Notas</h1>
+        <table class="table">
+            <thead>
                 <tr>
-                    <td>{{ $nota->nota1 }}</td>
-                    <td>{{ $nota->nota2 }}</td>
-                    <td>{{ $nota->nota3 }}</td>
-                    <td>{{ $nota->nota4 }}</td>
-                    <td>{{ $nota->promedio }}</td>
-                    <td>{{ $nota->parcial }}</td>
-                    <td>{{ $nota->alumno_id }}</td>
-                    <td>{{ $nota->cursos_id }}</td>
-                    <td>{{ $nota->profesor_id }}</td>
+                    <th>Nota 1</th>
+                    <th>Nota 2</th>
+                    <th>Nota 3</th>
+                    <th>Nota 4</th>
+                    <th>Promedio</th>
+                    <th>Parcial</th>
+                    <th>Alumno</th>
+                    <th>Curso</th>
+                    <th>Profesor</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    
-    <a href="{{ route('home') }}" class="btn btn-primary">Volver a Home</a>
-</div>
+            </thead>
+            <tbody>
+                @foreach ($notas as $nota)
+                    <tr>
+                        <td>{{ $nota->nota1 }}</td>
+                        <td>{{ $nota->nota2 }}</td>
+                        <td>{{ $nota->nota3 }}</td>
+                        <td>{{ $nota->nota4 }}</td>
+                        <td>{{ $nota->promedio }}</td>
+                        <td>{{ $nota->parcial }}</td>
+                        <td>{{ $nota->nombre }} {{ $nota->apellido }}</td>
+                        <td>{{ $nota->nombrecurso }}</td>
+                        <td>{{ $nota->nombre_profesor }} {{ $nota->apellido_profesor }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
